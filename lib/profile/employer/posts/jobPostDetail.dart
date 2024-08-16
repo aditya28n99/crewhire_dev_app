0d72b16cd1b90b5eb419327659c0c511387dd587
Aditya Sunil Wakale <adityawakale208@gmail.com>
2024-08-16 16:03:39 +0530

    Job details page beautified

--- lib/profile/employer/posts/jobPostDetail.dart
+++ lib/profile/employer/posts/jobPostDetail.dart
@@ -25,6 +25,7 @@ class _JobPostDetailState extends State<JobPostDetail> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Job Post Detail'),
+        // backgroundColor: Colors.blueGrey[800],
       ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: jobPostData,
@@ -37,45 +38,152 @@ class _JobPostDetailState extends State<JobPostDetail> {
             return Center(child: Text('No job post details available.'));
           } else {
             final jobPost = snapshot.data!;
+            final benefits = jobPost['benefits'] as List<dynamic>? ?? [];
+            final skills = jobPost['skills'] as List<dynamic>? ?? [];
 
             return SingleChildScrollView(
               padding: EdgeInsets.all(16.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    jobPost['jobTitle'] ?? 'No Title',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  // Job Title and Location
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          jobPost['jobTitle'] ?? 'No Title',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey[800], // Your matching color
+                          ),
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          jobPost['jobLocation'] ?? 'No Location',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blueGrey[600], // Your matching color
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 8.0),
-                  Text(
-                    'Job ID: ${jobPost['jobId'] ?? 'No ID'}',
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  SizedBox(height: 24.0),
+
+                  // Job Description
+                  _buildSection(
+                    title: 'Job Description',
+                    content: jobPost['jobDescription'] ?? 'No Description',
                   ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'Job Description:',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+
+                  // Job Requirements
+                  _buildSection(
+                    title: 'Job Requirements',
+                    content:
+                        jobPost['jobRequirements'] ?? 'No Requirements Info',
                   ),
-                  Text(jobPost['jobDescription'] ?? 'No Description'),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'Education Required:',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+
+                  // Responsibilities
+                  _buildSection(
+                    title: 'Responsibilities',
+                    content: 'Detailed responsibilities will be listed here.',
                   ),
-                  Text(jobPost['education'] ?? 'No Education Info'),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'Experience Required:',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+
+                  // Education Required
+                  _buildSection(
+                    title: 'Education Required',
+                    content: jobPost['education'] ?? 'No Education Info',
                   ),
-                  Text(jobPost['experience'] ?? 'No Experience Info'),
-                  // Add more details as needed
+
+                  // Experience Required
+                  _buildSection(
+                    title: 'Experience Required',
+                    content: jobPost['experience'] ?? 'No Experience Info',
+                  ),
+
+                  // Salary
+                  _buildSection(
+                    title: 'Salary',
+                    content: jobPost['salary'] ?? 'No Salary Info',
+                  ),
+
+                  // Employment Type and Work Type
+                  _buildSection(
+                    title: 'Employment Type',
+                    content:
+                        jobPost['employementType'] ?? 'No Employment Type Info',
+                  ),
+                  _buildSection(
+                    title: 'Work Type',
+                    content: jobPost['workType'] ?? 'No Work Type Info',
+                  ),
+
+                  // Skills Required
+                  _buildSection(
+                    title: 'Skills Required',
+                    content: skills.isEmpty
+                        ? 'No Skills Info'
+                        : skills.map((skill) => '• $skill').join('\n'),
+                  ),
+
+                  // Benefits
+                  _buildSection(
+                    title: 'Benefits',
+                    content: benefits.isEmpty
+                        ? 'No Benefits Info'
+                        : benefits.map((benefit) => '• $benefit').join('\n'),
+                  ),
+
+                  // Apply Before
+                  _buildSection(
+                    title: 'Apply Before',
+                    content: jobPost['applyBefore'] ?? 'No Apply Before Info',
+                  ),
+
+                  SizedBox(height: 40.0),
                 ],
               ),
             );
           }
         },
+      ),
+    );
+  }
+
+  Widget _buildSection({required String title, required String content}) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 16.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.blueGrey[800], // Your matching color
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            content,
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
       ),
     );
   }
